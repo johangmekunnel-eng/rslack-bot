af550e94b7993d7a87747e8ffafd828df4b42547
Johan Mekkunnel <johan@mac.mynetworksettings.com>
2026-06-28 02:58:32 -0400

    fix express wildcard crash on railway

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ app.get("/api/summaries/:userId", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "rmoney-dashboard/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "rmoney-dashboard/dist/index.html"));
 });
 
